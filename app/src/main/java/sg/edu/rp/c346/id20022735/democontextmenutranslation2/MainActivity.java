d317cdfca038b5a9287d99c7c465cd7851c2be8f
kw346 <20022735@myrp.edu.sg>
2021-06-01 13:18:34 +0800

    cfm finish.

--- app/src/main/java/sg/edu/rp/c346/id20022735/democontextmenutranslation2/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022735/democontextmenutranslation2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -12,7 +13,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvTranslatedText;
+    TextView tvTranslatedText2;
 
+    String wc ="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         tvTranslatedText = findViewById(R.id.textViewTT);
         registerForContextMenu(tvTranslatedText);
-
+        tvTranslatedText2 = findViewById(R.id.textView2);
+        registerForContextMenu(tvTranslatedText2);
     }
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,
@@ -28,14 +32,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         menu.add(0, 0, 0, "English");
         menu.add(0, 1, 1, "Italian");
+        if (v == tvTranslatedText){
+            wc = "hello";
+        }
+        else if (v == tvTranslatedText2){
+            wc ="bye";
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.EnglishSelection) {
+            tvTranslatedText.setText("Hello ");
+            tvTranslatedText2.setText("Bye");
+            return true;
+        } else if (id == R.id.italianSelection) {
+            tvTranslatedText.setText("Ciao");
+            tvTranslatedText2.setText("Addio");
+            return true;
+        } else {
+            tvTranslatedText.setText("Error translation");
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-
-            if(item.getItemId()==0) { //check whether the selected menu item ID is 0
+        if (wc.equalsIgnoreCase("hello")){
+            if(item.getItemId()==0) { //check whether the selected menu item ID s 0
                 //code for action
                 Toast.makeText(MainActivity.this, "English is chosen", Toast.LENGTH_SHORT).show();
                 tvTranslatedText.setText("Hello");
@@ -47,11 +80,23 @@ public class MainActivity extends AppCompatActivity {
                 tvTranslatedText.setText("Ciao");
                 return true;  //menu item successfully handled
             }
+        }
+        if (wc.equalsIgnoreCase("bye")){
+            if(item.getItemId()==0) { //check whether the selected menu item ID is 0
+                //code for action
+                Toast.makeText(MainActivity.this, "English is chosen", Toast.LENGTH_SHORT).show();
+                tvTranslatedText2.setText("Bye");
+                return true; //menu item successfully handled
+            }
+            else if(item.getItemId()==1) { //check if the selected menu item ID is 1
+                //code for action
+                Toast.makeText(MainActivity.this, "Italian is chosen", Toast.LENGTH_SHORT).show();
+                tvTranslatedText2.setText("Addio");
+                return true;  //menu item successfully handled
+            }
+        }
 
-
-
-        return super.onContextItemSelected(item);
+        return super.onContextItemSelected(item); //pass menu item to the superclass implementation
     }
-
 
 }
